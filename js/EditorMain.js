60024d72a5cf2cb69cb0daa93f7fd076d4870801
Mantas <Sonata@Sonatos_PC.home>
2015-11-24 20:55:54 +0200

    Integruotas piesimo langas

--- js/EditorMain.js
+++ js/EditorMain.js
@@ -6,9 +6,17 @@ $(document).ready(function() {
     patternImage.src = "imgs/patterns/pattern0.png";
 
     var colourChanger = null;
-    var productPreview = new ProductPreview("canvas", "patternCanvas", "imgs/sweater.png", patternImage, 1, "threejs");
+    var productPreview = new ProductPreview("canvas", "patternCanvas", "imgs/sweater.png", patternImage, 1, "webgl");
     var mouseHandler = new MouseHandler(productPreview);
-
+    
+    //Sukuriamas piesimo modulis
+    var lc = LC.init(document.getElementsByClassName('literally')[0],
+            {
+            imageURLPrefix: 'lib/img',
+            imageSize: {width: 500, height: 500}
+        }
+        );
+    
     $(window).load(function() {
         colourChanger = new ColourChanger(productPreview);
         getColours(productPreview.getPatternImageId());
@@ -26,6 +34,14 @@ $(document).ready(function() {
             }
         });
     }
+    
+    $(".literally").on("mouseup mousedown", function(){
+        var newImage = new Image();
+            newImage.src = lc.getImage().toDataURL("img/png", 1);
+            productPreview.setOriginalPatternImage(newImage);
+            productPreview.setOriginalSizePatternImage(newImage); 
+            productPreview.setPatternImage(newImage);
+    });
 
     $(".patternButton").click( function(e) {
         var buttonId = $(this).attr("buttonNumber");
@@ -34,7 +50,7 @@ $(document).ready(function() {
             var newImage = new Image();
             newImage.src = "imgs/patterns/pattern" + buttonId + ".png";
             productPreview.setOriginalPatternImage(newImage);
-            productPreview.setOriginalSizePatternImage(newImage);
+            productPreview.setOriginalSizePatternImage(newImage); 
             productPreview.setPatternImage(newImage);
             getColours(productPreview.getPatternImageId());
             colourChanged = null;
